6776b5c040515c796293a5e5d4d66d2a6352eae5
marijusiMac <marijuslau@yahoo.com>
2018-04-13 14:55:25 +0300

    Log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      flash[:success] = "Jūs prisijungėte"
+      flash.now[:success] = "Jūs prisijungėte"
+      log_in user
       redirect_to user
     else
       flash.now[:danger] = 'El. Pašto adresas/Slaptažodis neteisingi(-as)'
@@ -14,6 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    session.delete(:user_id)
+    @current_user = nil
+    redirect_to root_path
   end
 end
