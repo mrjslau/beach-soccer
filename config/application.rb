fc8cafab0d4605261363f22e60f3fb0f2118ef50
mrjslau <marijuslau@yahoo.com>
2018-04-17 18:57:54 +0300

    Devise setup

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,18 @@ Bundler.require(*Rails.groups)
 
 module BeachSoccer
   class Application < Rails::Application
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+        puts value
+      end if File.exists?(env_file)
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.force_ssl = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
