c728c3b2c5b60e69fe6086e0dfd0933741a0f567
marijusiMac <marijuslau@yahoo.com>
2018-04-17 19:58:31 +0300

    Sign-up, log-in/off with Devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user = User.find(params[:id])
   end
@@ -20,6 +22,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'teams#index'
-
   resources :teams
-  
   resources :users
-  get '/signup', to: 'users#new'
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
 end
